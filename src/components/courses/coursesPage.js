9854cea494c1f5512549d00783ad685dbbdca317
Amir Jafarian <amirali.jfr61@gmail.com>
2019-09-02 19:54:23 -0400

    feat(page-routes): add page and routes
    - add simple home page
    - add simple about page
    - add simple course page
    - add simple pageNotFound
    - add simple Nav component
    - routes

---
+++ src/components/courses/coursesPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// I use class here for now top have it have it statefull;
+// later on I refactor it to use React Hooks. ie. state and effect hooks
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
---
+++ src/components/pageNotFound/pageNotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (<div>
+    <h1>404</h1>
+    <p>Page does not exist</p>
+  </div>)
+};
+
+export default PageNotFound;
