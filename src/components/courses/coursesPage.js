5c9a703fd90ef15b51a90efc3d6de21a1d91ddf1
Amir Jafarian <amirali.jfr61@gmail.com>
2019-09-02 23:39:44 -0400

    feat(react): add course add functionality using state in page level

--- src/components/courses/coursesPage.js
+++ src/components/courses/coursesPage.js
@@ -3,10 +3,32 @@ import React from "react";
 // I use class here for now top have it have it statefull;
 // later on I refactor it to use React Hooks. ie. state and effect hooks
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+  handleOnChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+  handleOnSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
   render() {
     return (
       <div className="jumbotron">
         <h1>Courses</h1>
+        <h3>Course:</h3>
+        <form onSubmit={this.handleOnSubmit}>
+          <input
+            type="text"
+            onChange={this.handleOnChange}
+            value={this.state.course.title}
+          />
+          <input type="submit" value="Save" />
+        </form>
       </div>
     );
   }
